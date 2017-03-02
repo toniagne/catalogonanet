e1eb4685e2c9df58b5ce8547a2cab9efd30e8f02
toniagne <toniagne@gmail.com>
2017-03-02 17:03:18 -0300

    ALTERAÇÕES

--- www/js/app.js
+++ www/js/app.js
@@ -1045,8 +1045,8 @@ function pesqKey(e){
 }
 
 function pesquisaClick(){
-    pesquisaTmp = document.getElementById('buscaQuery');
-    var query = pesquisaTmp.textContent || el.innerText;
+    pesquisaTmp = document.getElementById('buscaQuery').value;
+    var query = document.getElementById('buscaQuery').value;
     document.getElementById('buscaQuery').value = "";
     var data = {
         busca : query
